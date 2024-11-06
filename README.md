55f90ab21eff715ce425d03afeec09e3622ce328
Devin <devinatkin@gmail.com>
2024-11-06 15:56:03 -0700

    Change script to add needed files to rc.local

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PiBitStreamer
 Connect a raspberry pi to 4 FPGAs and allow for remote bitstream programming. 
+
+- Enable I2C using the raspi-config tool.
+
+- Run the Setup Pi Bitstreamer script which should set the required rc.local file up to launch the required software at boot
+
+- To keep things up to date you may want to add git pull to the rc.local from within the repo directory
--- setup_pi_bitstreamer.sh
+++ setup_pi_bitstreamer.sh
@@ -7,7 +7,7 @@ echo "Setting up Pi BitStreamer..."
 sudo apt update
 sudo apt upgrade -y
 
-# Install required packages
+# Install required packages for website
 sudo apt install -y git
 sudo apt install -y npm
 
@@ -33,5 +33,34 @@ cmake --build .
 
 sudo make install
 
-npm install
-npm run start
+# Install required packages for small display
+python3 -m venv ~/PiBitStreamer/venv
+source ~/PiBitStreamer/venv/bin/activate
+
+pip3 install adafruit-circuitpython-ssd1306
+pip3 install pillow
+
+sudo apt install i2c-tools
+curl -O https://raw.githubusercontent.com/adafruit/Adafruit_CircuitPython_SSD1306/main/examples/ssd1306_stats.py
+
+# Get the current working directory
+SCRIPT_DIR=$(pwd)
+
+# Define the lines to be added to rc.local
+RC_LINES=$(cat <<EOF
+# Start PiBitStreamer server and frontend
+cd /home/dmatkin/PiBitStreamer
+sudo -u dmatkin /bin/bash -c "source /home/dmatkin/PiBitStreamer/venv/bin/activate && npm run start" &
+EOF
+)
+
+# Add the PiBitStreamer startup command if it doesn't already exist
+if ! grep -Fxq "Start PiBitStreamer server and frontend" /etc/rc.local; then
+    # Insert RC_LINES before 'exit 0'
+    sudo sed -i "s|exit 0|$RC_LINES\nexit 0|" /etc/rc.local
+    echo "Added PiBitStreamer startup command to /etc/rc.local"
+else
+    echo "Startup command already exists in /etc/rc.local"
+fi
+
+sudo reboot
